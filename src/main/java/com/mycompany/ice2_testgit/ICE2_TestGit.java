8567c2e4cc7e0e4b9623b8a36d8b3683b6870d29
nikhil-sde <nikhil3h@gmail.com>
2025-10-02 09:43:15 -0400

    rev 1 local

---
+++ src/main/java/com/mycompany/ice2_testgit/ICE2_TestGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice2_testgit;
+
+/**
+ *
+ * @author nikhil
+ */
+public class ICE2_TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("rev 1 local");
+    }
+}
